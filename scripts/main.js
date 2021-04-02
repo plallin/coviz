bc8a08b356b5b149a8d76dab8daaf1d45b6296d4
Pauline <pauline.lallinec@gmail.com>
2021-04-02 19:12:06 +0100

    added graph for total covid vax

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,8 @@ var margin = {top: 20, right: 50, bottom: 30, left: 50},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var parseDate = d3.time.format("%d-%b-%y").parse,
-    bisectDate = d3.bisector(function(d) { return d.date; }).left,
-    formatValue = d3.format(",.2f"),
-    formatCurrency = function(d) { return "$" + formatValue(d); };
+var parseDate = d3.time.format("%d %b %Y").parse,
+    bisectDate = d3.bisector(function(d) { return d.date; }).left;
 
 var x = d3.time.scale()
     .range([0, width]);
@@ -23,35 +21,42 @@ var yAxis = d3.svg.axis()
 
 var line = d3.svg.line()
     .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
+    .y(function(d) { return y(d.total); });
 
-var svg = d3.select("body").append("svg")
+var today = new Date();
+
+var svgTotalVax = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("data.tsv", function(error, data) {
+d3.csv("data.csv", function(error, data) {
   if (error) throw error;
 
+  // read data
   data.forEach(function(d) {
+    // console.log(d)
     d.date = parseDate(d.date);
-    d.close = +d.close;
+    d.total = +d.total;
   });
 
   data.sort(function(a, b) {
     return a.date - b.date;
   });
 
-  x.domain([data[0].date, data[data.length - 1].date]);
-  y.domain(d3.extent(data, function(d) { return d.close; }));
+  xAxisStart = data[0].date
+  
+  x.domain([xAxisStart, today]);
+  y.domain(
+      d3.extent(data, function(d) { return d.total; }));
 
-  svg.append("g")
+  svgTotalVax.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis);
 
-  svg.append("g")
+  svgTotalVax.append("g")
       .attr("class", "y axis")
       .call(yAxis)
     .append("text")
@@ -59,14 +64,14 @@ d3.tsv("data.tsv", function(error, data) {
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Price ($)");
+      .text("Total number of vaccines");
 
-  svg.append("path")
+  svgTotalVax.append("path")
       .datum(data)
       .attr("class", "line")
       .attr("d", line);
 
-  var focus = svg.append("g")
+  var focus = svgTotalVax.append("g")
       .attr("class", "focus")
       .style("display", "none");
 
@@ -77,7 +82,7 @@ d3.tsv("data.tsv", function(error, data) {
       .attr("x", 9)
       .attr("dy", ".35em");
 
-  svg.append("rect")
+  svgTotalVax.append("rect")
       .attr("class", "overlay")
       .attr("width", width)
       .attr("height", height)
@@ -91,7 +96,7 @@ d3.tsv("data.tsv", function(error, data) {
         d0 = data[i - 1],
         d1 = data[i],
         d = x0 - d0.date > d1.date - x0 ? d1 : d0;
-    focus.attr("transform", "translate(" + x(d.date) + "," + y(d.close) + ")");
-    focus.select("text").text(formatCurrency(d.close));
+    focus.attr("transform", "translate(" + x(d.date) + "," + y(d.total) + ")");
+    focus.select("text").text(d.date + " - " + d.total);
   }
 });
