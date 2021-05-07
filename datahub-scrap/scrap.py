123464eb1cf9f69789e4351fdae7e1c8c505cfa3
Pauline <pauline.lallinec@gmail.com>
2021-05-07 13:01:48 +0100

    enrich data scrapped from source

--- datahub-scrap/scrap.py
+++ datahub-scrap/scrap.py
@@ -17,7 +17,7 @@ date_query = {
     'where': f"{DATE_COL}>'2020-01-01 00:00:00'", # "Dates>'2020-01-01 00:00:00'",
     'returnGeometry': False,
     'spatialRel': 'esriSpatialRelIntersects',
-    'outFields': f'{DATE_COL},dateText',
+    'outFields': f'*',
     # 'orderByFields': f'{DATE_COL} asc',
     'resultOffset': 0,
     'resultRecordCount': 32000,
@@ -26,6 +26,24 @@ date_query = {
 
 res = requests.get(date, params=date_query)
 date_data = json.loads(res.text)['features'][0]['attributes']
+
+### Vaccines per providers (no Janssen yet)
+
+data_vax_per_type = 'https://services-eu1.arcgis.com/z6bHNio59iTqqSUY/arcgis/rest/services/Covid19_Vaccine_Administration_VaccineTypeHostedView_V2/FeatureServer/0/query'
+param_vax_per_type = {
+    'f': 'json',
+    'where': f"{DATE_COL}>'2020-01-01 00:00:00'", # "Dates>'2020-01-01 00:00:00'",
+    'returnGeometry': False,
+    'spatialRel': 'esriSpatialRelIntersects',
+    'outFields': f'*',
+    # 'orderByFields': f'{DATE_COL} asc',
+    'resultOffset': 0,
+    'resultRecordCount': 32000,
+    'resultType': 'standard'
+}
+
+res = requests.get(data_vax_per_type, params=param_vax_per_type)
+type_data = json.loads(res.text)['features'][0]['attributes']
 
 
 ### Vaccines per type of dose (first, second), cohort, and providers (no Janssen yet)
@@ -36,7 +54,7 @@ param_vax_all_types = {
     'where': f"{DATE_COL}>'2020-01-01 00:00:00'", # "Dates>'2020-01-01 00:00:00'",
     'returnGeometry': False,
     'spatialRel': 'esriSpatialRelIntersects',
-    'outFields': f'{DATE_COL},totalAdministered,firstDose,secondDose,az,modern,pf,cohort1,cohort1b,totalcoh1,cohort2,cohort2b,totalcoh2,cohort3,cohort3b,totalcoh3,cohort4,cohort4b,totalcoh4,cohort5,cohort5b,totalcoh5,cohort7,cohort7b,totalcoh7,othercohort1,othercohort2,totalcoh16',
+    'outFields': f'*',
     # 'orderByFields': f'{DATE_COL} asc',
     'resultOffset': 0,
     'resultRecordCount': 32000,
@@ -52,8 +70,10 @@ vax_data = json.loads(res.text)['features'][0]['attributes']
 data = open("datahub-scrap/data.json", "r").read()
 json_data = json.loads(data)
 reldate = str(vax_data['relDate'])
-json_data['data'][reldate] = vax_data
-json_data['data'][reldate]['dateText'] = date_data['dateText']
+json_data['data'][reldate]['Covid19_Vaccine_Administration_Text_Date_View'] = date_data
+json_data['data'][reldate]['Covid19_Vaccine_Administration_VaccineTypeHostedView_V2'] = type_data
+json_data['data'][reldate]['Covid19_Vaccine_Administration_Hosted_View'] = vax_data
+
 
 with open('datahub-scrap/data.json', 'w', encoding='utf-8') as f:
     json.dump(json_data, f, ensure_ascii=False, indent=4)
