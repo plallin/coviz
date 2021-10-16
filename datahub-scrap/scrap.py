807777e342fdbea12bb59ecf451d287139aba537
Pauline <pauline.lallinec@gmail.com>
2021-10-16 14:34:40 +0100

    upstream api change

--- datahub-scrap/scrap.py
+++ datahub-scrap/scrap.py
@@ -6,75 +6,61 @@
 import json
 import requests
 
-DATE_COL = 'relDate'
+### Daily vaccination data
 
-
-### Date
-
-date = 'https://services-eu1.arcgis.com/z6bHNio59iTqqSUY/arcgis/rest/services/Covid19_Vaccine_Administration_Text_Date_View/FeatureServer/0/query'
+date = 'https://services-eu1.arcgis.com/z6bHNio59iTqqSUY/arcgis/rest/services/COVID19_Daily_Vaccination/FeatureServer/0/query'
 date_query = {
     'f': 'json',
-    'where': f"{DATE_COL}>'2020-01-01 00:00:00'", # "Dates>'2020-01-01 00:00:00'",
+    'where': f"VaccinationDate>'2020-01-01 00:00:00'", # "Dates>'2020-01-01 00:00:00'",
     'returnGeometry': False,
     'spatialRel': 'esriSpatialRelIntersects',
     'outFields': f'*',
-    # 'orderByFields': f'{DATE_COL} asc',
+    'orderByFields': f'VaccinationDate asc',
     'resultOffset': 0,
     'resultRecordCount': 32000,
     'resultType': 'standard'
 }
 
 res = requests.get(date, params=date_query)
-date_data = json.loads(res.text)['features'][0]['attributes']
+daily_data = json.loads(res.text)['features']
 
 
-### Vaccines per providers (no Janssen yet)
+## Weekly vaccination data
 
-data_vax_per_type = 'https://services-eu1.arcgis.com/z6bHNio59iTqqSUY/arcgis/rest/services/Covid19_Vaccine_Administration_VaccineTypeHostedView_V2/FeatureServer/0/query'
+data_vax_per_type = 'https://services-eu1.arcgis.com/z6bHNio59iTqqSUY/arcgis/rest/services/COVID19_Weekly_Vaccination_Figures/FeatureServer/0/query'
 param_vax_per_type = {
     'f': 'json',
-    'where': f"{DATE_COL}>'2020-01-01 00:00:00'", # "Dates>'2020-01-01 00:00:00'",
+    'where': f"ExtractDate>'2020-01-01 00:00:00'", # "Dates>'2020-01-01 00:00:00'",
     'returnGeometry': False,
     'spatialRel': 'esriSpatialRelIntersects',
     'outFields': f'*',
-    # 'orderByFields': f'{DATE_COL} asc',
+    'orderByFields': f'ExtractDate asc',
     'resultOffset': 0,
     'resultRecordCount': 32000,
     'resultType': 'standard'
 }
 
 res = requests.get(data_vax_per_type, params=param_vax_per_type)
-type_data = json.loads(res.text)['features'][0]['attributes']
-
-
-### Vaccines per type of dose (first, second), cohort, and providers (no Janssen yet)
-
-data_vax_all = 'https://services-eu1.arcgis.com/z6bHNio59iTqqSUY/arcgis/rest/services/Covid19_Vaccine_Administration_Hosted_View/FeatureServer/0/query'
-param_vax_all_types = {
-    'f': 'json',
-    'where': f"{DATE_COL}>'2020-01-01 00:00:00'", # "Dates>'2020-01-01 00:00:00'",
-    'returnGeometry': False,
-    'spatialRel': 'esriSpatialRelIntersects',
-    'outFields': f'*',
-    # 'orderByFields': f'{DATE_COL} asc',
-    'resultOffset': 0,
-    'resultRecordCount': 32000,
-    'resultType': 'standard'
-}
-
-res = requests.get(data_vax_all, params=param_vax_all_types)
-vax_data = json.loads(res.text)['features'][0]['attributes']
+# print(res.text)
+weekly_data = json.loads(res.text)['features']
 
 
 ### save data
 
 data = open("datahub-scrap/data.json", "r").read()
 json_data = json.loads(data)
-reldate = str(vax_data['relDate'])
-json_data['data'][reldate] = {}
-json_data['data'][reldate]['Covid19_Vaccine_Administration_Text_Date_View'] = date_data
-json_data['data'][reldate]['Covid19_Vaccine_Administration_VaccineTypeHostedView_V2'] = type_data
-json_data['data'][reldate]['Covid19_Vaccine_Administration_Hosted_View'] = vax_data
+
+for d in daily_data:
+    date = d['attributes']['VaccinationDate']
+    if date not in json_data['data']:
+        json_data['data'][date] = {}
+    json_data['data'][date]['COVID19_Daily_Vaccination'] = d['attributes']
+
+for d in weekly_data:
+    date = d['attributes']['ExtractDate']
+    if date not in json_data['data']:
+        json_data['data'][date] = {}
+    json_data['data'][date]['COVID19_Weekly_Vaccination_Figures'] = d['attributes']
 
 
 with open('datahub-scrap/data.json', 'w', encoding='utf-8') as f:
