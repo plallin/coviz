5e4364b992bf2d6ea5a7919aa2a3195e360cfd38
Pauline <pauline.lallinec@gmail.com>
2021-10-18 22:47:56 +0100

    upstream API change; updating json format for data dump file

--- datahub-scrap/scrap.py
+++ datahub-scrap/scrap.py
@@ -6,6 +6,7 @@
 import json
 import requests
 
+
 ### Daily vaccination data
 
 date = 'https://services-eu1.arcgis.com/z6bHNio59iTqqSUY/arcgis/rest/services/COVID19_Daily_Vaccination/FeatureServer/0/query'
@@ -49,18 +50,24 @@ weekly_data = json.loads(res.text)['features']
 data = open("datahub-scrap/data.json", "r").read()
 json_data = json.loads(data)
 
+
+# apis = [
+#     "Covid19_Vaccine_Administration_Text_Date_View",  # retired
+#     "Covid19_Vaccine_Administration_VaccineTypeHostedView_V2",  # retired
+#     "Covid19_Vaccine_Administration_Hosted_View",  # retired
+#     "COVID19_Daily_Vaccination",  # active
+#     "COVID19_Weekly_Vaccination_Figures",  # active
+# ]
+
 for d in daily_data:
     date = str(d['attributes']['VaccinationDate'])
-    if date not in json_data['data']:
-        json_data['data'][date] = {}
-    json_data['data'][date]['COVID19_Daily_Vaccination'] = d['attributes']
+    json_data['COVID19_Daily_Vaccination']['data'][date] = d['attributes']
 
 for d in weekly_data:
     date = str(d['attributes']['Week'])
-    if date not in json_data['data']:
-        json_data['data'][date] = {}
-    json_data['data'][date]['COVID19_Weekly_Vaccination_Figures'] = d['attributes']
+    json_data['COVID19_Weekly_Vaccination_Figures']['data'][date] = d['attributes']
 
 
 with open('datahub-scrap/data.json', 'w', encoding='utf-8') as f:
     json.dump(json_data, f, ensure_ascii=False, indent=4)
+
