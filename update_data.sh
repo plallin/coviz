050476dbb7433f1cf861e53a9426a90ffbe9f2da
Pauline <pauline.lallinec@gmail.com>
2021-12-20 23:46:40 +0000

    update script

--- update_data.sh
+++ update_data.sh
@@ -10,7 +10,6 @@ git add datahub-scrap/data.json
 
 git add data.csv data-sources/vaccines.csv data-sources/comments.csv
 git commit -m "New data as of: `date`"
-git push origin main
 
 ./fetch_latest_api_data.py
 git add datahub-scrap/*.json
